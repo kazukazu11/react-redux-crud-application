40a9cd8f96b69591a8091688456e076a03a623dc
岩本一真 <iwamotokazuma@iwamotokazumanoMacBook-Pro.local>
2020-03-10 00:00:03 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 /*jsxのコーディングの際はReactの文字列が必要,逆にjsのコーディングの際はReactの文字列が不必要*/
-import React,{Component} from 'react';
+//import React,{Component} from 'react';
 
-class App extends Component{
-	render(){
-		// const greeting = "Hi,Tom!"
-		// const dom = <h1 className="foo">{greeting}</h1>;
-		// return dom;
-		/* inputの枠内をクリックすると、コンソールにI am clickedが表示*/
-		// return <input type="text" onClick={()=>{console.log("I am clicked")}}/>;
-		/* inputの枠内に文字を入力すると、コンソールにI am clickedが表示*/
-		// <input type="text" onChange={()=>{console.log("I am clicked")}}/>;
-		return (
-			/*reactの制約として、returnで返すjsxは一つのタグで無ければならない*/
-			// <div>
-			// 	<label htmlFor="bar">bar</label>
-			// 	<input type="text" onChange={()=>{console.log("I am clicked")}}/>
-			// </div>
-			/*こうする事で一つのタグに認識されるものの、最終的に画面に表示されるHTMLには、divが存在しない＝＝余計なタグを存在させなくて済む*/
-			<React.Fragment>
-				<label htmlFor="bar">bar</label>
-				<input type="text" onChange={()=>{console.log("I am clicked")}}/>
-			</React.Fragment>
-		)
-	}
+/*Appの事をクラスコンポーネントと言う*/
+// class App extends Component{
+// 	render(){
+// 		// const greeting = "Hi,Tom!"
+// 		// const dom = <h1 className="foo">{greeting}</h1>;
+// 		// return dom;
+// 		/* inputの枠内をクリックすると、コンソールにI am clickedが表示*/
+// 		// return <input type="text" onClick={()=>{console.log("I am clicked")}}/>;
+// 		/* inputの枠内に文字を入力すると、コンソールにI am clickedが表示*/
+// 		// <input type="text" onChange={()=>{console.log("I am clicked")}}/>;
+// 		return (
+// 			reactの制約として、returnで返すjsxは一つのタグで無ければならない
+// 			// <div>
+// 			// 	<label htmlFor="bar">bar</label>
+// 			// 	<input type="text" onChange={()=>{console.log("I am clicked")}}/>
+// 			// </div>
+// 			/*こうする事で一つのタグに認識されるものの、最終的に画面に表示されるHTMLには、divが存在しない＝＝余計なタグを存在させなくて済む*/
+// 			<React.Fragment>
+// 				<label htmlFor="bar">bar</label>
+// 				<input type="text" onChange={()=>{console.log("I am clicked")}}/>
+// 			</React.Fragment>
+// 		)
+// 	}
+// }
+
+import React from 'react';
+
+/*関数コンポーネント*/
+const App = () =>{
+	return  (
+		<div>
+			<Cat />
+			<Cat />
+			<Cat />
+			<Cat />
+		</div>
+	)
+}
+
+const Cat = () =>{
+	return  <div>Meow!</div>
 }
 
 export default App;
