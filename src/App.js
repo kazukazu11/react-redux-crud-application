7e9f241cd2fc2a6a29feb55c348b018c730ebc52
岩本一真 <iwamotokazuma@iwamotokazumanoMacBook-Pro.local>
2020-03-09 22:12:36 +0900

    more-jsx

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-//import React from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-
-// function App() {
-//   return <div><h1>Hello,world!</h1></div>;
-// }
-
+/*jsxのコーディングの際はReactの文字列が必要,逆にjsのコーディングの際はReactの文字列が不必要*/
 import React,{Component} from 'react';
-
-/*jsxのコーディング*/
-// class App extends Component {
-//   render(){
-//     return <div>Hello,world!</div>;
-//   }
-// }
-
-/*jsxが使われる理由は⬆︎⬇︎を比べた時に⬆︎の方が直感的に分かりやすい為、最終的な成果物の形式により近い構文で表現できる*/
-
-/*jsのコーディング*/
-// class App extends Component {
-//   render(){
-//     return React.createElement(
-//     	"div",
-//     	null,
-//     	"Hello, world!!"
-//     )
-//   }
-// }
 
 class App extends Component{
 	render(){
-		return <h1>Hello, world!</h1>;
+		// const greeting = "Hi,Tom!"
+		// const dom = <h1 className="foo">{greeting}</h1>;
+		// return dom;
+		/* inputの枠内をクリックすると、コンソールにI am clickedが表示*/
+		// return <input type="text" onClick={()=>{console.log("I am clicked")}}/>;
+		/* inputの枠内に文字を入力すると、コンソールにI am clickedが表示*/
+		// <input type="text" onChange={()=>{console.log("I am clicked")}}/>;
+		return (
+			/*reactの制約として、returnで返すjsxは一つのタグで無ければならない*/
+			// <div>
+			// 	<label htmlFor="bar">bar</label>
+			// 	<input type="text" onChange={()=>{console.log("I am clicked")}}/>
+			// </div>
+			/*こうする事で一つのタグに認識されるものの、最終的に画面に表示されるHTMLには、divが存在しない＝＝余計なタグを存在させなくて済む*/
+			<React.Fragment>
+				<label htmlFor="bar">bar</label>
+				<input type="text" onChange={()=>{console.log("I am clicked")}}/>
+			</React.Fragment>
+		)
 	}
 }
 
