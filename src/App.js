0949cc6792f35fb3e5a151c57e0a58b3987c421c
岩本一真 <iwamotokazuma@iwamotokazumanoMacBook-Pro.local>
2020-04-04 00:27:17 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types'  //propの型を定義してあげる
+import React, { Component } from 'react';
 
-const App = () =>{
-	const profiles = [
-		{name:"Taro",age:10},
-		{name:"Hanako",age:5},
-		{name:"NoName",age:3}
-	]
-	return  (
-		<div>
-			{
-				profiles.map((profile, index)=>{
-					return <User name={profile.name} age={profile.age} key={index}/>
-				})
-			}
-		</div>
-	)
-}
+const App = () => (<Counter></Counter>)
 
-const User = (props) =>{
-	return  <div>Hi!,I am {props.name}, and {props.age} years old!</div>
-}
+class Counter extends Component{
+	//constructorは初期化処理で実行されるメソッド
+	constructor(props){
+		super(props)
+		this.state = { count: 0}  //counteをkeyとして、値が0という意味
+	}
 
-//ここでpropの型を定義してあげる
-User.propTypes = {
-	name: PropTypes.string,
-	age: PropTypes.number.isRequired    //.isRequiredを付けると、今回の場合、ageに値が入ってなかったらエラーになる。
+	handlePlusButton = () => {
+		//this.state = { count: this.state.count + 1}のように、
+		//stateを直接いじっては駄目。setStateを使いなさい
+		//何故かというと、setStateが実行されるとrenderが実行される。
+		this.setState({ count: this.state.count + 1 })
+	}
+
+	handleMinusButton = () => {
+		this.setState({ count: this.state.count - 1 })
+	}
+
+	render(){
+		return(
+			<React.Fragment>
+				<div>count: { this.state.count }</div>
+				<button onClick={this.handlePlusButton}>+1</button>
+				<button onClick={this.handleMinusButton}>-1</button>
+			</React.Fragment>
+		)
+	}
 }
 
 export default App;
